ae863232a7999c7f833ce1c50f60f9bfab00c20c
Samuel Des Cormiers <sdcormiers@hotmail.com>
2024-09-23 09:26:53 -0400

    Atelier 11 V0.0.0

---
+++ Atelier11/code.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+app.get('/', function (req, res) {
+    res.render('pages/index', {title: 'Hello World', message: 'Hello World!'});
+});
+
+app.get('/about', function (req, res) {
+    res.render('pages/about', {title: 'About', message: 'About page'});
+});
+
+app.get('/annee/:annee/:mois', function(req, res) {
+    res.render('./pages/annee.ejs', {annee: req.params.annee, mois: req.params.mois});
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+});
+
+app.listen(8080);
+
+console.log('Server running at port 8080');
+
